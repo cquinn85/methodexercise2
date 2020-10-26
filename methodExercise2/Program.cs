2eb1a3829d0e9b20bd6e66649becce946595d59d
cquinn85 <cequinn300@gmail.com>
2020-10-26 12:26:31 -0400

    method exercise 2 omplete

---
+++ methodExercise2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace methodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is: {sum}");
+
+            Console.WriteLine("Give me a number to multiply");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to multiply to the first one.");
+            int y = int.Parse(Console.ReadLine());
+
+            int product = Multiply(x, y);
+            Console.WriteLine($"The product is {product}.");
+
+
+
+        }
+
+
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
